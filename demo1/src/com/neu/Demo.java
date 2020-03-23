1e7392cc4b1ef559aa558138fb412a5981a495fe
ZSL807044811 <34212957+ZSL807044811@users.noreply.github.com>
2020-03-23 16:02:12 +0800

    张世林的IDEA

--- demo1/src/com/neu/Demo.java
+++ demo1/src/com/neu/Demo.java
@@ -4,13 +4,13 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hi 你好啊");
 	}
-	public static void demo1(){
-		System.out.println("IDEA好！！！！！");
-	}
 	public static void demo2(){
 		System.out.println("IDEA好！！！！！");
 	}
 	public static void demo3(){
+		System.out.println("IDEA好！！！！！");
+	}
+	public static void demo4(){
 		System.out.println("IDEA好！！！！！");
 	}
 }
